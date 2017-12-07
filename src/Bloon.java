4b1560ee711ad9bbaa113aa3e3d4647c6628b970
MistriHD <jmistri7@gmail.com>
2017-12-06 22:59:50 -0800

    Adding bloon stuff

--- src/Bloon.java
+++ src/Bloon.java
@@ -1,11 +1,49 @@
 
-public abstract class Bloon {
+public class Bloon {
 	public String type;
-	private int layers;
 	private int[] coordinates;
-	public int delay;
+	public int layers;
 	public int currentDelay;
 	public String needsToSummonNextBloon;
+	
+	// Red: 1 layer, speed 2000
+	// Blue: 2 layers, speed 1600
+	// Green: 3 layers, speed 1200
+	// Yellow: 4 layers, speed 800
+	// Pink: 5 layers, speed 400
+	// Cyan (small): 6 layers, speed 300
+	// Black (small): 7 layers, speed 300
+	// Clay: 8 layers, speed 1500
+	// Lead: 9 layers, speed 2000
+	
+	public Bloon(String s) {
+		switch (s) {
+			case ("red"): layers = 1;
+			case ("blue"): layers = 2;
+			case ("green"): layers = 3;
+			case ("yellow"): layers = 4;
+			case ("pink"): layers = 5;
+			case ("cyan"): layers = 6;
+			case ("black"): layers = 7;
+			case ("clay"): layers = 8;
+			case ("lead"): layers = 9;
+		}
+	}
+	
+	public int getSpeed() {
+		switch (layers) {
+			case (1): return 2000;
+			case (2): return 1600;
+			case (3): return 1200;
+			case (4): return 800;
+			case (5): return 400;
+			case (6): return 300;
+			case (7): return 300;
+			case (8): return 1500;
+			case (9): return 2000;
+			default: return -1;
+		}
+	}
 	
 	public int[] getCoordinates() {
 		return coordinates;
@@ -22,7 +60,7 @@ public abstract class Bloon {
 		if (coordinates == null) return;
 		
 		currentDelay += BloonsWindow.FPSDelay;
-		if (currentDelay >= delay) {
+		if (currentDelay >= getSpeed()) {
 			currentDelay = 0;
 			
 			if (needsToSummonNextBloon == null) {
@@ -45,10 +83,8 @@ public abstract class Bloon {
 	}
 	
 	public void pop() {
-		BloonsRunner.money ++;
 		layers --;
-		if (layers == 0) {
-			coordinates = new int[] {-1, -1};
-		}
+		BloonsRunner.money ++;
+		if (layers == 0) coordinates = new int[]{-1, -1};
 	}
 }
--- src/RedBloon.java
+++
@@ -1,4 +0,0 @@
-
-public class RedBloon extends Bloon {
-
-}
